888fa7aea0102c0540bffaf57bbde2126ce907fc
ritishakotapati <74190725+ritishakotapati@users.noreply.github.com>
2021-01-11 12:10:43 +0530

    Add files via upload

---
+++ Pendulum.js
@@ -0,0 +1,35 @@
+class Pendulum {
+    constructor(x,y,color){
+    
+    var options= {
+        restitution:1,
+        friction:0,
+        frictionAir:0.0,
+        slop:1,
+        inertia:Infinity,
+        
+    
+    }
+    
+    this.x=x;
+            this.y=y;
+            this.color=color
+            this.body = Bodies.rectangle(x, y, 40, 40, options); 
+    World.add(world,this.body);
+    
+    }
+    display(){
+        var pos=this.body.position; 
+        var angle=this.body.angle;
+        push() 
+        translate(pos.x, pos.y);
+        rotate(angle)
+         noStroke()
+         fill(this.color) 
+         ellipse(0,0,60,60); 
+         pop()
+    
+    }
+    
+    }
+    
